b459f0e0cb17517e6f9233f2434fc91a0f4db90e
Jordan <jormit2000@gmail.com>
2021-01-06 13:32:46 +1100

    Confirmed build config with sine synth example

--- src/JSynth.cpp
+++ src/JSynth.cpp
@@ -1,12 +1,45 @@
 ﻿// JSynth.cpp : Defines the entry point for the application.
 //
 
-#include "JSynth.h"
+#include <q/support/literals.hpp>
+#include <q/synth/sin.hpp>
+#include <q_io/audio_stream.hpp>
 
-using namespace std;
+///////////////////////////////////////////////////////////////////////////////
+// Synthesize a 440 Hz sine wave.
+///////////////////////////////////////////////////////////////////////////////
+
+namespace q = cycfi::q;
+using namespace q::literals;
+
+struct synth : q::port_audio_stream
+{
+    synth(q::frequency freq)
+        : port_audio_stream(0, 2)
+        , phase(freq, this->sampling_rate())
+    {}
+
+    void process(out_channels const& out)
+    {
+        auto left = out[0];
+        auto right = out[1];
+        for (auto frame : out.frames())
+        {
+            // Synthesize the sin wave
+            right[frame] = left[frame] = q::sin(phase++);
+        }
+    }
+
+    q::phase_iterator phase;   // The phase iterator
+};
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    synth my_synth{ 880_Hz };
+
+    my_synth.start();
+    q::sleep(5_s);
+    my_synth.stop();
+
+    return 0;
 }
